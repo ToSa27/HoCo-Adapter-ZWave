eaef795ef5307f189450cae44dfdf30103ceb0df
ToSa27 <tobias@die-hoffs.net>
2017-01-01 15:21:29 +0100

    limited initialization functionality

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var config = require("./config.js");
-var logger = require("./log.js");
-var mqtt = require("./mqtt.js");
+var log = require("./log.js");
+var Bus = require("./bus.js");
+var bus;
 
-var bus = new mqtt(config, "zwave", "12345678");
+var Zwave = require("./zwave.js");
+var gw = new Zwave(config.zwave);
 
-//var zwave = require("./zwave.js");
-//var gw = 
+gw.on("connected", function(homeid) {
+	bus = new Bus(config.mqtt, "zwave", homeid.toString(16));
+
+	bus.on("connected", () => {
+		log.info("bus connected");
+	});
+
+	bus.on("adapter", (command, message) => {
+	        log.info("bus adapter command: " + command + ": " + message);
+	});
+
+	bus.on("node", (nodeid, command, message) => {
+	        log.info("bus node command: " + command + " for " + nodeid + ": " + message);
+	});
+
+	bus.on("parameter", (nodeid, parameterid, command, message) => {
+	        log.info("bus parameter command: " + command + " for " + nodeid + "/" + parameterid + ": " + message);
+	});
+});
+
---
+++ zwave.js
@@ -0,0 +1,47 @@
+const EventEmitter = require('events');
+const util = require('util');
+const log = require("./log.js");
+const ZwaveLib = require('openzwave-shared');
+
+var _homeid;
+var _zwave;
+var _config;
+var _connected = false;
+
+function Zwave(config) {
+	log.info("zwave config: " + JSON.stringify(config));
+	EventEmitter.call(this);
+	var self = this;
+	_config = config;
+	_zwave = new ZwaveLib({
+		ConsoleOutput: false,
+		Logging: false,
+		SaveConfiguration: false,
+		DriverMaxAttempts: 3,
+		PollInterval: 500,
+		SuppressValueRefresh: true
+	});
+	_zwave.connect(_config.device);
+	
+	_zwave.on('driver ready', function(homeid) {
+		log.info("zwave driver ready");
+		_connected = true;
+		self.emit("connected", homeid);
+	});
+
+	_zwave.on('driver failed', function() {
+                log.info("zwave driver failed");
+		_connected = false;
+		_zwave.disconnect();
+		self.emit("disconnected");
+	});
+
+	_zwave.on('scan complete', function() {
+                log.info("zwave scan complete");
+// ToDo: emit "ready" here???
+	});
+};
+
+util.inherits(Zwave, EventEmitter)
+
+module.exports = Zwave;
