b23a180db17ea20498b30f27bb9f75a1e2765bfa
ToSa27 <tobias@die-hoffs.net>
2017-01-07 15:53:47 +0100

    allow predefined device

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 cd "${0%/*}"
 . ${HOCO_HOME}/data/config.sh
 export HOCO_ZWAVE_OZW_VERSION=1.4.1
-export HOCO_ZWAVE_DEVICE=/dev/ttyACM0
+if [ -z "$HOCO_ZWAVE_DEVICE" ]; then 
+  export HOCO_ZWAVE_DEVICE=/dev/ttyACM0
+fi
 sudo apt-get install -y libudev-dev
 wget http://old.openzwave.com/downloads/openzwave-${HOCO_ZWAVE_OZW_VERSION}.tar.gz
 tar zxvf openzwave-*.gz
